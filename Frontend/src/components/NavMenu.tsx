4e098b9704f7b6ec601fe6c2f8763d36e0fba94e
Nahuel Stella <nahuelprg@gmail.com>
2024-05-25 15:35:47 -0300

    Ajustes a la Autenticación
    -Cerrar Sesión
    -Redirección al inicio de sesión

--- Frontend/src/components/NavMenu.tsx
+++ Frontend/src/components/NavMenu.tsx
@@ -3,6 +3,7 @@ import { Popover, Transition } from '@headlessui/react'
 import { Bars3Icon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom'
 import { User } from '../types'
+import { useQueryClient } from '@tanstack/react-query'
 
 type NavMenuProps = {
     name: User['name']
@@ -10,6 +11,12 @@ type NavMenuProps = {
 
 export default function NavMenu({ name }: NavMenuProps) {
 
+    const queryClient = useQueryClient();
+    const logout = () => {
+        localStorage.removeItem('AUTH_TOKEN');
+        queryClient.invalidateQueries({queryKey: ['user']});
+    }
+    
     return (
         <Popover className="relative">
             <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400">
@@ -39,7 +46,7 @@ export default function NavMenu({ name }: NavMenuProps) {
                 <button
                     className='block p-2 hover:text-purple-950'
                     type='button'
-                    onClick={() => { }}
+                    onClick={logout}
                 >
                     Cerrar Sesión
                 </button>
